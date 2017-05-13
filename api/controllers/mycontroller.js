18382440e5d3f0e5706c1dffcc2e7b3969d3cdd5
Lorenzo Maiorfi <maiorfi@innovactive.it>
2017-05-13 14:56:30 +0200

    Metodo put che processa il body

--- api/controllers/mycontroller.js
+++ api/controllers/mycontroller.js
@@ -5,3 +5,34 @@ exports.get_foo_by_id = function (req, res) {
         "fooId": req.params.fooId
     });
 };
+
+exports.put_foo = function (req, res) {
+
+    if (!req.body.payload) {
+        res.status(401).send("L'oggetto passato non ha la proprietà payload");
+        return;
+    }
+
+    if (!req.body.payload.stringa) {
+        res.status(401).send("L'oggetto passato non ha la proprietà payload.stringa");
+        return;
+    }
+
+     if (typeof req.body.payload.stringa !== "string") {
+        res.status(401).send("La proprietà 'payload.stringa' non è di tipo stringa");
+        return;
+    }
+
+    var foo={
+        "fooId": getRandomId(1, 1000),
+        "payload": req.body.payload
+    };
+
+    console.info(foo);
+
+    res.json(foo);
+};
+
+function getRandomId(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
--- api/routes/myroutes.js
+++ api/routes/myroutes.js
@@ -3,7 +3,9 @@
 module.exports = function (app) {
     var myController = require('../controllers/mycontroller');
 
-    // todoList Routes
     app.route('/foo/:fooId')
         .get(myController.get_foo_by_id);
+
+    app.route('/foo')
+        .put(myController.put_foo);
 };
